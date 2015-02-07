7bf41456fdd3b0fbd8fc212c7a08f0434a8a5956
TheU5erPL <theu5erpl@gmail.com>
2015-02-07 09:32:29 +0100

    Random Chunk Generating

--- Assets/Skrypty/Chunk.cs
+++ Assets/Skrypty/Chunk.cs
@@ -28,7 +28,7 @@ public class Chunk : MonoBehaviour {
 			for (int z = 0; z < World.activeWorld.ch_width; z++)
 			{
 				map[x, 0, z] = 1;
-				map[x, 1, z] = (byte)Random.Range (0,1);
+				map[x, 1, z] = (byte)Random.Range (0,2);
 			}
 		}
 		GenerateMesh();
@@ -51,17 +51,29 @@ public class Chunk : MonoBehaviour {
 						continue;
 					byte block = map[x,y,z];
 					
-					//bok 1
-					GenerateFace(block, new Vector3(x,y,z), Vector3.up, Vector3.forward, false, verts, UVs, tris);
-					GenerateFace(block, new Vector3(x + 1,y,z), Vector3.up, Vector3.forward, true, verts, UVs, tris);
+					// lewa sciana
+					if( Visible (x - 1, y, z))
+						GenerateFace(block, new Vector3(x,y,z), Vector3.up, Vector3.forward, false, verts, UVs, tris);
 					
-					//bok 2
-					GenerateFace(block, new Vector3(x,y,z), Vector3.up, Vector3.right, true, verts, UVs, tris);
-					GenerateFace(block, new Vector3(x,y,z + 1), Vector3.up, Vector3.right, false, verts, UVs, tris);
+					// prawa sciana
+					if( Visible (x + 1, y, z))
+						GenerateFace(block, new Vector3(x + 1,y,z), Vector3.up, Vector3.forward, true, verts, UVs, tris);
 					
-					// gora
-					GenerateFace(block, new Vector3(x,y,z), Vector3.forward, Vector3.right, false, verts, UVs, tris);
-					GenerateFace(block, new Vector3(x, y + 1, z), Vector3.forward, Vector3.right, true, verts, UVs, tris);
+					// tyl
+					if ( Visible (x, y, z - 1))
+						GenerateFace(block, new Vector3(x,y,z), Vector3.up, Vector3.right, true, verts, UVs, tris);
+					
+					// przod
+					if ( Visible (x, y, z + 1))
+						GenerateFace(block, new Vector3(x,y,z + 1), Vector3.up, Vector3.right, false, verts, UVs, tris);
+					
+					// podloga
+					if ( Visible (x, y - 1, z))
+						GenerateFace(block, new Vector3(x,y,z), Vector3.forward, Vector3.right, false, verts, UVs, tris);
+					
+					// sufit
+					if ( Visible (x, y + 1, z))
+						GenerateFace(block, new Vector3(x, y + 1, z), Vector3.forward, Vector3.right, true, verts, UVs, tris);
 				}
 			}
 		}
@@ -107,4 +119,28 @@ public class Chunk : MonoBehaviour {
 		}
 	}
 	
+	public virtual bool Visible (int x, int y, int z)
+	{
+		byte block = GetBlock(x,y,z);
+		switch (block)
+		{
+		default:
+		case 0:
+			return true;
+		case 1:
+			return false;
+		}
+	}
+	
+	public virtual byte GetBlock(int x, int y, int z)
+	{
+		if ( 
+		    (x >= World.activeWorld.ch_width) ||
+		    (y >= World.activeWorld.ch_height) ||
+		    (z >= World.activeWorld.ch_width) ||
+		    ( x < 0) || ( y < 0) || (z < 0)
+		    )
+			return 0;
+		return map[x,y,z];
+	}
 }
